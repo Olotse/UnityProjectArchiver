aa7ea61be4f3ec0437c05b18e042f57a5821527b
Olotse <contact@devloader.fr>
2022-12-11 14:50:49 +0100

    Ajout de l'icône en haut de la fenêtre et modification du titre de la fenêtre

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <QApplication>
 #include "projectwindow.h"
 
-//#include "iconablebutton.h"
-
 int main(int argc, char **argv)
 {
 	QApplication app(argc, argv);
-
-	/*IconableButton button;
-	button.show();*/
+	app.setWindowIcon(QIcon(":/icons/mainicon"));
 
 	ProjectWindow projectWindow;
-	projectWindow.show();
+	projectWindow.setWindowTitle("Unity Project Archiver");
 
+	projectWindow.show();
 	return app.exec();
 }
